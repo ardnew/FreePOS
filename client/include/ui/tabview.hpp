fa6802ccc46fe47a18c83d201c99abb1c4beaf37
ardnew <andrew@ardnew.com>
2023-09-15 00:14:33 -0500

    remove unused include

--- client/include/ui/tabview.hpp
+++ client/include/ui/tabview.hpp
@@ -1,21 +1,22 @@
 #ifndef ui_tabview_hpp
 #define ui_tabview_hpp
 
-#include "../spec.hpp"
 #include "../ipc.hpp"
-#include "../icons.h"
+#include "../spec.hpp"
 
 #include "chart.hpp"
 #include "weight.hpp"
 
-class TabView: public View {
+class TabView : public View {
 private:
   typedef void (*scroll_begin_t)(lv_event_t *);
   static inline void scroll_begin(lv_event_t *e) {
     // Disable the scroll animations. Triggered when a tab button is clicked
     if (lv_event_get_code(e) == LV_EVENT_SCROLL_BEGIN) {
       lv_anim_t *a = (lv_anim_t *)lv_event_get_param(e);
-      if (a) { a->time = 0; }
+      if (a) {
+        a->time = 0;
+      }
     }
   }
 
@@ -26,23 +27,22 @@ protected:
   Weight _weight;
 
 public:
-
   bool init(lv_obj_t *root) override {
     _base = lv_tabview_create(root, LV_DIR_LEFT, 80);
 
-    lv_obj_add_event_cb(
-      lv_tabview_get_content(_base),
-      static_cast<scroll_begin_t>(&TabView::scroll_begin),
-      LV_EVENT_SCROLL_BEGIN, NULL);
+    lv_obj_add_event_cb(lv_tabview_get_content(_base),
+                        static_cast<scroll_begin_t>(&TabView::scroll_begin),
+                        LV_EVENT_SCROLL_BEGIN, NULL);
 
     lv_obj_set_style_bg_color(_base, lv_palette_lighten(LV_PALETTE_RED, 2), 0);
 
     lv_obj_t *tab_btns = lv_tabview_get_tab_btns(_base);
-    lv_obj_set_style_bg_color(tab_btns, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
-    lv_obj_set_style_text_color(tab_btns, lv_palette_lighten(LV_PALETTE_GREY, 5), 0);
-    lv_obj_set_style_border_side(tab_btns, LV_BORDER_SIDE_RIGHT, LV_PART_ITEMS | LV_STATE_CHECKED);
-
-    // lv_obj_set_style_text_font(_base, &mug_24, 0);
+    lv_obj_set_style_bg_color(tab_btns, lv_palette_darken(LV_PALETTE_GREY, 3),
+                              0);
+    lv_obj_set_style_text_color(tab_btns,
+                                lv_palette_lighten(LV_PALETTE_GREY, 5), 0);
+    lv_obj_set_style_border_side(tab_btns, LV_BORDER_SIDE_RIGHT,
+                                 LV_PART_ITEMS | LV_STATE_CHECKED);
 
     _chart.init(lv_tabview_add_tab(_base, "Chart"));
     _weight.init(lv_tabview_add_tab(_base, "Weight"));
